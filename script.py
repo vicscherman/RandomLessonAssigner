791e43c4a1c57ca193647539f9abf6343c7de570
vicscherman <vicscherman@gmail.com>
2021-02-19 21:36:34 -0500

    initial commit

---
+++ script.py
@@ -0,0 +1,15 @@
+
+import random
+
+
+number = random.randint(0, 29)
+
+lessons = ['String Reversal', 'Palindromes', 'Integer Reversal', 'MaxChars', 'FizzBuzz', 'Array Chunking', 'Anagrams', 'Sentence Capitalization',
+'Printing steps', 'Pyramids', 'Find the vowels','Matrix Spiral', 'Runtime Complexity', 'Fibonacci Sequences', 'The Queue', 'Underwater queue weaving',
+'Stack em with stacks', 'Two become one','Linked Lists', 'Circular lists', 'Step back from the tail', 'building a tree', 'tree width and level width',
+'Binary Search Trees','Validating Binary Search Trees', 'Javascript events', 'Design Questions', 'Bubble Sort', 'Selection Sort', 'Merge sort']
+
+print(f"Today's Lesson is: {number+3}.",lessons[number])
+
+
+
